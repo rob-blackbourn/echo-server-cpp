67310db735a3db65981fd5b41bd4ebce5e1be784
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-09-27 10:59:25 +0100

    More work on addresses

--- 08 - Addresses/address_for_name.cpp
+++ 08 - Addresses/address_for_name.cpp
@@ -8,18 +8,49 @@
 #include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
 
-int main()
+std::string to_string(std::uint16_t value)
 {
-    const char* host = "yahoo.com";
-    const std::uint16_t port = 22000;
-    char port_str[6];
-    std::memset(port_str, 0, sizeof(port_str));
-    auto res = std::to_chars(port_str, port_str + sizeof(port_str) - 1, port);
+    char buf[6];
+    std::memset(buf, 0, sizeof(buf));
+    auto res = std::to_chars(buf, buf + sizeof(buf) - 1, value);
     if (res.ec != std::errc{})
     {
         throw std::system_error(std::make_error_code(res.ec));
     }
+    return std::string(static_cast<const char*>(buf));
+}
+
+std::string to_string(const in_addr& addr)
+{
+    char ip_address[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &addr, ip_address, sizeof(ip_address)) == nullptr)
+    {
+        throw std::system_error(errno, std::generic_category(), "failed to convert ip address");
+    }
+    return (std::string(static_cast<const char*>(ip_address)));
+}
+
+std::string to_string(const sockaddr_in& addr)
+{
+    return to_string(addr.sin_addr) + ":" + to_string(ntohs(addr.sin_port));
+}
+
+std::ostream& operator << (std::ostream& os, const sockaddr_in& addr)
+{
+    return os << to_string(addr);
+}
+
+void example1()
+{
+    const char* host = "yahoo.com";
+    const std::uint16_t port = 22000;
+
+    auto port_str = to_string(port);
 
     addrinfo hints {
         .ai_flags = 0,
@@ -32,7 +63,7 @@ int main()
         .ai_next = nullptr
     };
     addrinfo* addresses;
-    int result = getaddrinfo(host, port_str, &hints, &addresses);
+    int result = getaddrinfo(host, port_str.c_str(), &hints, &addresses);
     if (result != 0)
     {
         throw std::runtime_error(gai_strerror(result));
@@ -41,16 +72,60 @@ int main()
     for (addrinfo* i = addresses; i != nullptr; i = i->ai_next)
     {
         auto in_addr = reinterpret_cast<sockaddr_in*>(i->ai_addr);
-
-        char ip_address[INET_ADDRSTRLEN];
-        if (inet_ntop(i->ai_family, &in_addr->sin_addr, ip_address, sizeof(ip_address)) == nullptr)
-        {
-            throw std::system_error(errno, std::generic_category(), "failed to convert ip address");
-        }
-        std::cout << "found " << ip_address << ":" << ntohs(in_addr->sin_port) << std::endl;
+        std::cout << "found " << to_string(*in_addr) << std::endl;
     }
 
     freeaddrinfo(addresses);
+}
+
+std::vector<sockaddr_in> getaddrinfo_inet4(const std::string& host, std::uint16_t port)
+{
+    std::vector<sockaddr_in> results;
+
+    auto port_str = to_string(port);
+
+    addrinfo hints {
+        .ai_flags = 0,
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+        .ai_protocol = IPPROTO_TCP,
+        .ai_addrlen = 0,
+        .ai_addr = nullptr,
+        .ai_canonname = nullptr,
+        .ai_next = nullptr
+    };
+    addrinfo* addresses;
+    int result = getaddrinfo(host.c_str(), port_str.c_str(), &hints, &addresses);
+    if (result != 0)
+    {
+        throw std::runtime_error(gai_strerror(result));
+    }
+
+    for (addrinfo* i = addresses; i != nullptr; i = i->ai_next)
+    {
+        results.push_back(*reinterpret_cast<sockaddr_in*>(i->ai_addr));
+    }
+
+    freeaddrinfo(addresses);
+
+    return results;
+}
+
+void example2()
+{
+    std::string host = "yahoo.com";
+    std::uint16_t port = 443;
+
+    auto addresses = getaddrinfo_inet4(host, port);
+    std::cout << "Address for " << host  << " on port " << port << std::endl;
+    for (const auto& address : addresses)
+        std::cout << address << std::endl;
+}
+
+int main()
+{
+    // example1();
+    example2();
 
     return 0;
 }
