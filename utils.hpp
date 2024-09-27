98cf4e05de60f271164a48a49d055ecb8a02ad64
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-09-27 07:41:23 +0100

    wip

--- utils.hpp
+++ utils.hpp
@@ -4,23 +4,36 @@
 #include <deque>
 #include <iostream>
 #include <set>
-#include <sstream>
+#include <span>
 #include <vector>
+
+
+template <typename InputIt>
+inline std::ostream& stream_out_collection(
+    std::ostream& os,
+    const std::string& opening_bracket,
+    const std::string& closing_bracket,
+    const std::string& item_separator,
+    const InputIt& first,
+    const InputIt& last)
+{
+    os << opening_bracket;
+
+    for (auto item = first; item != last; ++item)
+    {
+        if (item != first)
+            os << item_separator;
+        os << *item;
+    }
+    os << closing_bracket;
+
+    return os;
+}
 
 template<typename T>
 inline std::ostream& operator << (std::ostream& os, const std::deque<T>& collection)
 {
-    os << "(";
-
-    for (auto item = collection.begin(); item != collection.end(); ++item)
-    {
-        if (item != collection.begin())
-            os << ",";
-        os << *item;
-    }
-    os << ")";
-
-    return os;
+    return stream_out_collection(os, "(", ")", ",", collection.begin(), collection.end());
 }
 
 template<typename T>
@@ -34,17 +47,7 @@ inline std::string to_string(const std::deque<T>& collection)
 template<typename T>
 inline std::ostream& operator << (std::ostream& os, const std::set<T>& collection)
 {
-    os << "{";
-
-    for (auto item = collection.begin(); item != collection.end(); ++item)
-    {
-        if (item != collection.begin())
-            os << ",";
-        os << *item;
-    }
-    os << "}";
-
-    return os;
+    return stream_out_collection(os, "{", "}", ",", collection.begin(), collection.end());
 }
 
 template<typename T>
@@ -58,26 +61,25 @@ inline std::string to_string(const std::set<T>& collection)
 template<typename T>
 inline std::ostream& operator << (std::ostream& os, const std::vector<T>& collection)
 {
-    os << "[";
-
-    for (auto item = collection.begin(); item != collection.end(); ++item)
-    {
-        if (item != collection.begin())
-            os << ",";
-        os << *item;
-    }
-    os << "]";
-
-    return os;
-}
-
-inline std::ostream& operator << (std::ostream& os, bool b)
-{
-    return os << (b ? "<true>" : "<false>");
+    return stream_out_collection(os, "[", "]", ",", collection.begin(), collection.end());
 }
 
 template<typename T>
 inline std::string to_string(const std::vector<T>& collection)
+{
+    std::stringstream ss;
+    ss << collection;
+    return ss.str();
+}
+
+template<typename T>
+inline std::ostream& operator << (std::ostream& os, const std::span<T>& collection)
+{
+    return stream_out_collection(os, "<", ">", ",", collection.begin(), collection.end());
+}
+
+template<typename T>
+inline std::string to_string(const std::span<T>& collection)
 {
     std::stringstream ss;
     ss << collection;
