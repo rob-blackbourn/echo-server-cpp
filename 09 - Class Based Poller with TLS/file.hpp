514e2f24988a301b15a05cb20e82c2150a29f9a3
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-10-03 10:12:02 +0100

    Split out file and socket

---
+++ 09 - Class Based Poller with TLS/file.hpp
@@ -0,0 +1,106 @@
+#ifndef JETBLACK_NET_FILE_HPP
+#define JETBLACK_NET_FILE_HPP
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <iostream>
+#include <system_error>
+
+namespace jetblack::net
+{
+
+  class File
+  {
+  protected:
+    int fd_;
+    bool is_open_ { true };
+
+  public:
+    explicit File(int fd) noexcept
+      : fd_(fd)
+    {
+    }
+
+    File(const File&) = delete;
+    File& operator = (const File&) = delete;
+
+    File(File&& other)
+    {
+      *this = std::move(other);
+    }
+
+    File& operator = (File&& other)
+    {
+      fd_ = other.fd_;
+      is_open_ = other.is_open_;
+
+      other.fd_ = -1;
+      other.is_open_ = false;
+
+      return *this;
+    }
+
+    ~File()
+    {
+      if (!is_open_)
+      {
+        try
+        {
+          close();
+          is_open_ = false;
+        }
+        catch(const std::exception& e)
+        {
+        }
+      }
+    }
+
+    int fd() const noexcept { return fd_; }
+
+    bool is_open() const noexcept { return is_open_; }
+    void is_open(bool value) { is_open_ = value; }
+    
+    void close()
+    {
+      int result = ::close(fd_);
+      if (result == -1) {
+        throw std::system_error(errno, std::generic_category(), "failed to close socket");
+      }
+    }
+
+    int fcntl_flags() const
+    {
+      int flags = ::fcntl(fd_, F_GETFL, 0);
+      if (flags == -1) {
+        throw std::system_error(errno, std::generic_category(), "fcntl failed to get flags");
+      }
+      return flags;
+    }
+
+    void fcntl_flags(int flags)
+    {
+      if (::fcntl(fd_, F_SETFL, flags) == -1) {
+        throw std::system_error(errno, std::generic_category(), "fcntl failed to set flags");
+      }
+    }
+
+    void fcntl_flag(int flag, bool is_add)
+    {
+      int flags = fcntl_flags();
+      flags = is_add ? (flags & ~flag) : (flags | flag);
+      fcntl_flags(flags);
+    }
+
+    bool blocking() const { return (fcntl_flags() & O_NONBLOCK) == O_NONBLOCK; }
+    void blocking(bool is_blocking) { fcntl_flag(O_NONBLOCK, is_blocking); }
+  };
+
+}
+
+#endif // JETBLACK_NET_FILE_HPP
--- 09 - Class Based Poller with TLS/tcp_socket.hpp
+++ 09 - Class Based Poller with TLS/tcp_socket.hpp
@@ -12,18 +12,16 @@
 #include <iostream>
 #include <system_error>
 
+#include "file.hpp"
+
 namespace jetblack::net
 {
 
-  class TcpSocket
+  class TcpSocket : public File
   {
-  protected:
-    int fd_;
-    bool is_open_ { true };
-
   public:
     explicit TcpSocket()
-      : fd_(socket(AF_INET, SOCK_STREAM, 0))
+      : File(socket(AF_INET, SOCK_STREAM, 0))
     {
       if (fd_ == -1) {
         throw std::system_error(
@@ -32,71 +30,9 @@ namespace jetblack::net
     }
 
     explicit TcpSocket(int fd) noexcept
-      : fd_(fd)
+      : File(fd)
     {
     }
-
-    TcpSocket(const TcpSocket&) = delete;
-    TcpSocket(TcpSocket&&) = delete;
-    TcpSocket& operator = (const TcpSocket&) = delete;
-    TcpSocket& operator = (TcpSocket&&) = delete;
-
-    ~TcpSocket()
-    {
-      if (!is_open_)
-      {
-        try
-        {
-          close();
-          is_open_ = false;
-        }
-        catch(const std::exception& e)
-        {
-        }
-      }
-    }
-
-    int fd() const noexcept { return fd_; }
-
-    bool is_open() const noexcept { return is_open_; }
-    void is_open(bool value) { is_open_ = value; }
-    
-    void close()
-    {
-      int result = ::close(fd_);
-      if (result == -1) {
-        throw std::system_error(
-          errno, std::generic_category(), "failed to close socket");
-      }
-    }
-
-    int fcntl_flags() const
-    {
-      int flags = ::fcntl(fd_, F_GETFL, 0);
-      if (flags == -1) {
-        throw std::system_error(
-          errno, std::generic_category(), "fcntl failed to get flags");
-      }
-      return flags;
-    }
-
-    void fcntl_flags(int flags)
-    {
-      if (::fcntl(fd_, F_SETFL, flags) == -1) {
-        throw std::system_error(
-          errno, std::generic_category(), "fcntl failed to set flags");
-      }
-    }
-
-    void fcntl_flag(int flag, bool is_add)
-    {
-      int flags = fcntl_flags();
-      flags = is_add ? (flags & ~flag) : (flags | flag);
-      fcntl_flags(flags);
-    }
-
-    bool blocking() const { return (fcntl_flags() & O_NONBLOCK) == O_NONBLOCK; }
-    void blocking(bool is_blocking) { fcntl_flag(O_NONBLOCK, is_blocking); }
 
     void set_option(int level, int name, bool is_set) const
     {
